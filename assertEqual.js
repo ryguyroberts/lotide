26fe4133ee2bca4c579eae98ad9e32a23d50c1a7
Ryan Robertson <ryguyrobertson@gmail.com>
2024-01-08 14:36:07 -0800

    Removed string concats, user correct assertion not assertation

--- assertEqual.js
+++ assertEqual.js
@@ -1,13 +1,14 @@
 // function to compare two input parameters and confirm if equal or not.
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("✅Assertation Passed: " + actual + " === " + expected);
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("🤢Assertation Failed: " + actual + " !== " + expected);
+    console.log(`🤢Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Lighthouse Labs", "Lighthouse Labs")
 assertEqual(1,1);
 assertEqual(2,3);
