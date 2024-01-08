e37de0ffd2c7641ef87e173ca2a0b109ab37006a
Ryan Robertson <ryguyrobertson@gmail.com>
2024-01-08 14:32:22 -0800

    Changed function name / swapped else if to just else

--- assertEqual.js
+++ assertEqual.js
@@ -1,13 +1,13 @@
 // function to compare two input parameters and confirm if equal or not.
-const AssertEqual = function(actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log("✅Assertation Passed: " + actual + " === " + expected);
-  } else if (actual !== expected) {
+  } else {
     console.log("🤢Assertation Failed: " + actual + " !== " + expected);
   }
 };
 
 // TEST CODE
-AssertEqual("Lighthouse Labs", "Bootcamp");
-AssertEqual(1,1);
-AssertEqual(2,3);
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
+assertEqual(2,3);
