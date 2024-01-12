6c0101c5ee6a967ac27a3c2aa77c790eac7a5fe0
Ryan Robertson <ryguyrobertson@gmail.com>
2024-01-11 17:22:16 -0800

    removed value variable input directly into callbacks

--- findKey.js
+++ findKey.js
@@ -21,9 +21,8 @@ const data1 = {
 const findKey = function(objInput, callback) {
   // Create array of keys
   let keyArr = Object.keys(objInput);
-  for (let key of keyArr) { 
-    let value = objInput[key];
-    if(callback(value)) {
+  for (let key of keyArr) { ;
+    if(callback(objInput[key])) {
       return key;
     } 
   }
