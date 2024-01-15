f3889a902367787e61381e49a32faf5b124e992f
Ryan Robertson <ryguyrobertson@gmail.com>
2024-01-15 12:21:03 -0800

    Created index.js to export all utility functions as object

---
+++ index.js
@@ -0,0 +1,24 @@
+// index.js
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const countLetters = require('./countLetters')
+const countOnly = require('./countOnly');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  map: map,
+  takeUntil: takeUntil,
+  without: without
+};
