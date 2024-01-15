a78f1f1095053c55b2885bf44416cf970806b148
Ryan Robertson <ryguyrobertson@gmail.com>
2024-01-15 07:47:27 -0800

    Refactored AssertEqual and Head and created test directory

--- assertEqual.js
+++ assertEqual.js
@@ -1,14 +1,10 @@
 // function to compare two input parameters and confirm if equal or not.
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`🤢Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual("Lighthouse Labs", "Lighthouse Labs")
-assertEqual(1,1);
-assertEqual(2,3);
+module.exports = assertEqual;
