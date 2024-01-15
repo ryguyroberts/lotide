636db7210a608277ba2be2e66f14154b66bdeaf9
Ryan Robertson <ryguyrobertson@gmail.com>
2024-01-15 09:09:08 -0800

    Small change to eqArray, refactored AssertArrayseEqual, created test for AssertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,29 +1,9 @@
-const eqArrays = function(compArray1, compArray2) {
-  //Default true
-  let currentTest = true;
-
-  // Auto fail if not same length
-  if (compArray1.length !== compArray2.length) {
-    currentTest = false;
-  } else {
-    // Loop and compare each member of both arrays.
-    for (let i = 0; i < compArray1.length; i++) {
-      // if same var becomes true else falses
-      if (compArray1[i] !== compArray2[i]) {
-        currentTest = false;
-        break;
-      }
-    }
-  }
-  return currentTest;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(ray1, ray2) {
   // If eqArray true. Passed, or if false Failed.
-  eqArrays(ray1, ray2) ? console.log(`✅Assertion Passed: ${ray1} === ${ray2}`) : console.log(`🤢Assertion Failed: ${ray1} !== ${ray2}`);
+  eqArrays(ray1, ray2) ? console.log(`✅✅✅Assertion Passed: ${ray1} === ${ray2}`) : console.log(`🛑🛑🛑Assertion Failed: ${ray1} !== ${ray2}`);
 };
 
-//test 
 
-assertArraysEqual([1,2,3],[1,2,3]);
-assertArraysEqual([1,2,2],[1,2,3]);
+module.exports = assertArraysEqual;
