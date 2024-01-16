d3d1d48cb9920857614a1b6c3f019d6b3529a789
Ryan Robertson <ryguyrobertson@gmail.com>
2024-01-15 16:46:55 -0800

    Made test cases for eqArray.test.js more than just a console.log

--- test/eqArrays.Test.js
+++ test/eqArrays.Test.js
@@ -1,6 +1,16 @@
 const _ = require('../index');
+const assert = require('chai').assert;
+
+describe("eqArrays", () => {
+  it("If Array 1 is `[1, 2, 3]` and Array 2 is `[1, 2, 3]` return true" , () => {
+    assert.strictEqual(_.eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+  it("If Array 1 is `[1, 2, 3]` and Array 2 is `[1, 2]` return false" , () => {
+    assert.strictEqual(_.eqArrays([1, 2, 3], [1, 2]), false);
+  });
+});
 
 
 // TEST CODE
-_.assertEqual(_.eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-_.assertEqual(_.eqArrays([1, 2, 3], [1, 2, 2]), false);
+// _.assertEqual(_.eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+// _.assertEqual(_.eqArrays([1, 2, 3], [1, 2, 2]), false);
